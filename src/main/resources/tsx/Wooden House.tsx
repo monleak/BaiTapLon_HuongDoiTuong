59a876bec16c1ed0389910ea129af2f650fbdbce
hahunavth <vuthanhha.2001@gmail.com>
2022-06-11 19:51:24 +0700

    new map

--- src/main/resources/tsx/Wooden House.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Wooden House" tilewidth="16" tileheight="16" tilecount="35" columns="7">
- <image source="oolt-project/src/main/resources/sprout-lands-sprites/tilesets/Wooden House.png" width="112" height="80"/>
-</tileset>
