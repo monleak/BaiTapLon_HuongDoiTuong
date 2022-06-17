037de6995aed6c3aef13c34b01343d43cd1f65d8
Nguyen Van An <91582513+ToilaAn@users.noreply.github.com>
2022-06-17 21:27:07 +0700

    Create Pig.java

---
+++ src/main/java/model/Animals/Pig.java
@@ -0,0 +1,54 @@
+package model.Animals;
+
+import model.*;
+import model.Activities.Schedule;
+
+public class Pig extends Animal {
+
+    // NOTE: This constructor only use for testing
+    public Pig () {
+        this.maxCalo    = 800;
+        this.maxHP      = 300;
+        this.maxSleep   = 200;
+        this.maxWater   = 100;
+
+        this.setWater(100);
+        this.setCalo(800);
+        this.setHP(300);
+        this.setSleep(200);
+        this.setAge(0);
+
+        this.neededFood = new FoodInventory(new Food("Fish", 10), 100);
+        this.activity = null;
+//        this.schedule = new Schedule();
+    }
+
+    public Pig (Schedule s, Food f, int foodAmount) {
+        this.maxCalo    = 800;
+        this.maxHP      = 300;
+        this.maxSleep   = 200;
+        this.maxWater   = 100;
+
+        this.setWater(100);
+        this.setCalo(800);
+        this.setHP(300);
+        this.setSleep(200);
+        this.setAge(0);
+
+        this.neededFood = new FoodInventory(f, foodAmount);
+        this.activity = null;
+        this.schedule = s;
+    }
+
+    @Override
+    public void growUp() {
+        getNeededFood().setAmount(getNeededFood().getAmount() + 10);
+
+        this.maxCalo    += 20;
+        this.maxHP      += 20;
+        this.maxSleep   += 20;
+        this.maxWater   += 20;
+
+        this.setAge(this.getAge() + 1);
+    }
+}
